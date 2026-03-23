1a4f68f3da218a3ea32b04a1625f0399b02aba0f
borh77 <borisahrnjez@gmail.com>
2026-03-23 19:57:45 +0100

    feat: add docker file

--- src/Solution.API/Program.cs
+++ src/Solution.API/Program.cs
@@ -1,5 +1,8 @@
+using Microsoft.EntityFrameworkCore;
 using Solution.API.Middleware;
 using Solution.API.Startup;
+// Dodaj using za tvoj DB context - proveri da li je putanja tačna
+using Solution.UniLeague.Infrastructure.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +15,27 @@ builder.Services.RegisterModules();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<UniLeagueContext>();
+
+        if (context.Database.EnsureCreated())
+        {
+            Console.WriteLine("TABELE SU USPEŠNO KREIRANE IZ KODA!");
+        }
+        else
+        {
+            Console.WriteLine("Tabele već postoje ili su migracije već odrađene.");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"GREŠKA: {ex.Message}");
+    }
+}
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
 if (app.Environment.IsDevelopment())
